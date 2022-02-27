0326e1f17b1625b182495faa77da838dfcc408e9
Pawel W <pawiercioch@gmail.com>
2022-02-27 15:09:54 +0100

    Move handling file upload on client purely side to simulation_form.js

--- mastersheet/static/js/simulation_form.js
+++ mastersheet/static/js/simulation_form.js
@@ -1,13 +1,26 @@
+var read_file = document.getElementById("read");
+var columns = document.getElementById("columns");
+var names_fields = document.getElementById("names");
+var df_fields = document.getElementById("df");
+var drag_fields = document.getElementById("drag");
+
+const df_form = df_fields.innerHTML;
+const drag_form = drag_fields.innerHTML;
+
+const files_form = '<b>File general info: <input type="file" name="general"></b>' +
+              '<b>File with downforce: <input type="file" name="df"></b>' +
+              '<b>File with drag: <input type="file" name="drag"></b>'
+
 function hideContent()
 {
     if (read_file.checked) {
-        columns.innerHTML = names_div;
+        df_fields.innerHTML = files_form;
+        drag_fields.innerHTML = "";
         hidden = true;
-        files.style.visibility = "visible";
     }
     else {
-        columns.innerHTML = names_div + df_div + drag_div;
+        df_fields.innerHTML = df_form;
+        drag_fields.innerHTML = drag_form;
         hidden = false;
-        files.style.visibility = "hidden";
     }
 }
